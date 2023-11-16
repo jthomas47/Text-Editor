f1f000461e49c5127fa23ab2f7f0c405aab853ae
jthomas47 <jackspurthomas@gmail.com>
2023-11-15 22:57:08 -0600

    deleted comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,23 +12,23 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   const textDb= await openDB('jate', 1);
   const tx = textDb.transaction('jate', 'readwrite');
 
-  // Open up the desired object store.
+  
   const store = tx.objectStore('jate');
 
-  // Use the .add() method on the store and pass in the content.
+  
   const request = store.put({ id:1, value:content });
 
-  // Get confirmation of the request.
+  
   const result = await request;
   console.log('data saved', result.value);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+
 export const getDb = async () => { 
   const textDb= await openDB('jate', 1);
   const tx = textDb.transaction('jate', 'readonly');
